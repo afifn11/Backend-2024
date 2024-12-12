afd45ea62ebc66d3eda9d85db84430bc36797aa4
Muhammad Afif Naufal <muhammadafifnaufal3@gmail.com>
2024-12-12 13:34:03 +0700

    menambahkan contoh synchronous

---
+++ synchronous.js
@@ -0,0 +1,9 @@
+console.log("Afif Membuka Bowser Google Chrome.");
+
+console.log(`
+Afif download file selama 1 jam.
+  Downloading 1 Hour ...
+  Download Complete.
+`);
+
+console.log("Afif membuka Youtube.");
